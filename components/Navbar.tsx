34f37001ad553000f313b94e6a7ffb3b26b1dc53
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-02-15 15:47:17 +0300

    NavBar

---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Layout from './ui/Layout'
+
+const Navbar = () => {
+  return (
+    <Layout>
+      <nav className="px-auto z-30 flex items-center justify-between">
+        <a href="/">
+          <img src="/images/logo.svg" alt="Logo" />
+        </a>
+        <div className=" flex justify-between space-x-4 text-secondary">
+          <a className="  hover:text-green" href="/">
+            Home
+          </a>
+          <a className="  hover:text-green" href="/">
+            About
+          </a>
+          <a className="  hover:text-green" href="/">
+            Contact
+          </a>
+          <a className="  hover:text-green" href="/">
+            Blog
+          </a>
+          <a className="  hover:text-green" href="/">
+            Careers
+          </a>
+        </div>
+        <div className="rounded-full border-0 bg-gradient-to-r from-green1 to-green2 py-2 px-6  text-white hover:bg-gradient-to-l  ">
+          <a href="/">Request Invite</a>
+        </div>
+      </nav>
+    </Layout>
+  )
+}
+
+export default Navbar
