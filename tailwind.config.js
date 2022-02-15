350c0bb5af81889d01e423767d6a4262786bb7cc
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-02-15 15:47:00 +0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,37 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Public Sans', 'ui-sans-serif', 'system-ui', -'apple-system'],
+      },
+      keyframes: {
+        fade: {
+          '0%': {
+            transform: 'translateY(-50px)',
+
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        fade: 'fade 1s ease-out both',
+      },
+    },
+    colors: {
+      white: '#ffffff',
+      green: '#30C88F',
+      primary: '#2D314D',
+      secondary: '#9597A5',
+      green1: '#33D35E',
+      green2: '#2AB6D9',
+      background1: '#FAFAFA',
+      background2: '#F4F5F7',
+    },
   },
   plugins: [],
 }
