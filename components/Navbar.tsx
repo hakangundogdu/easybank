0f3a05d5c4114b125b75c9c05efc5f26e7d32d45
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-02-15 21:49:47 +0300

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,32 @@
 import React from 'react'
-import Layout from './ui/Layout'
 
 const Navbar = () => {
   return (
-    <Layout>
-      <nav className="px-auto z-30 flex items-center justify-between">
-        <a href="/">
-          <img src="/images/logo.svg" alt="Logo" />
+    <nav className="flex items-center justify-between px-40 py-7">
+      <a href="/">
+        <img src="/images/logo.svg" alt="Logo" />
+      </a>
+      <div className=" flex justify-between space-x-8 text-secondary">
+        <a className="  hover:text-primary" href="/">
+          Home
         </a>
-        <div className=" flex justify-between space-x-8 text-secondary">
-          <a className="  hover:text-primary" href="/">
-            Home
-          </a>
-          <a className="  hover:text-primary" href="/">
-            About
-          </a>
-          <a className="  hover:text-primary" href="/">
-            Contact
-          </a>
-          <a className="  hover:text-primary" href="/">
-            Blog
-          </a>
-          <a className="  hover:text-primary" href="/">
-            Careers
-          </a>
-        </div>
-        <div className=" rounded-full bg-gradient-to-r from-green1 to-green2 py-2  px-6 text-white hover:bg-gradient-to-l ">
-          <a href="/">Request Invite</a>
-        </div>
-      </nav>
-    </Layout>
+        <a className="  hover:text-primary" href="/">
+          About
+        </a>
+        <a className="  hover:text-primary" href="/">
+          Contact
+        </a>
+        <a className="  hover:text-primary" href="/">
+          Blog
+        </a>
+        <a className="  hover:text-primary" href="/">
+          Careers
+        </a>
+      </div>
+      <div className=" rounded-full bg-gradient-to-r from-green1 to-green2 py-2  px-6 text-white hover:bg-gradient-to-l ">
+        <a href="/">Request Invite</a>
+      </div>
+    </nav>
   )
 }
 
--- components/ui/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Layout = (props) => {
-  return <div className="px-40 py-7 ">{props.children}</div>
-}
-
-export default Layout
