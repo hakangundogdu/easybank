d8661160bf84a7e20f332c76d1fce43fd1104ae6
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2022-02-16 13:25:17 +0300

    .

--- components/Layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Layout = (props: any) => {
-  return <div className="px-12 lg:px-32">{props.children}</div>
-}
-
-export default Layout
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,50 +1,47 @@
 import React from 'react'
-import Layout from './layout'
 
 const Navbar = () => {
   return (
-    <Layout>
-      <nav className="flex items-center justify-between  py-7 ">
-        <a href="/">
-          <img src="/images/logo.svg" alt="Logo" />
+    <nav className="flex items-center justify-between px-12 py-7 lg:px-32 ">
+      <a href="/">
+        <img src="/images/logo.svg" alt="Logo" />
+      </a>
+      <div className=" flex justify-between space-x-8 text-secondary">
+        <a
+          className=" border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
+          href="/"
+        >
+          Home
         </a>
-        <div className=" flex justify-between space-x-8 text-secondary">
-          <a
-            className=" border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
-            href="/"
-          >
-            Home
-          </a>
-          <a
-            className="  border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
-            href="/about"
-          >
-            About
-          </a>
-          <a
-            className=" border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
-            href="/"
-          >
-            Contact
-          </a>
-          <a
-            className=" border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
-            href="/"
-          >
-            Blog
-          </a>
-          <a
-            className="  border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
-            href="/"
-          >
-            Careers
-          </a>
-        </div>
-        <div className=" rounded-full bg-gradient-to-r from-green1 to-green2 py-2  px-6 text-white hover:bg-gradient-to-l ">
-          <a href="/">Request Invite</a>
-        </div>
-      </nav>
-    </Layout>
+        <a
+          className="  border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
+          href="/about"
+        >
+          About
+        </a>
+        <a
+          className=" border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
+          href="/"
+        >
+          Contact
+        </a>
+        <a
+          className=" border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
+          href="/"
+        >
+          Blog
+        </a>
+        <a
+          className="  border-b-2 border-white transition duration-300 hover:border-b-2 hover:border-green hover:text-primary"
+          href="/"
+        >
+          Careers
+        </a>
+      </div>
+      <div className=" rounded-full bg-gradient-to-r from-green1 to-green2 py-2  px-6 text-white hover:bg-gradient-to-l ">
+        <a href="/">Request Invite</a>
+      </div>
+    </nav>
   )
 }
 
